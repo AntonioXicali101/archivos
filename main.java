04d6485f91b64ce073859cb0e8515c40fa8a1ed2
AntonioXicali101 <122307799+AntonioXicali101@users.noreply.github.com>
2023-03-17 22:28:11 -0600

    Add files via upload

---
+++ main.java
@@ -0,0 +1,15 @@
+package archivos;
+
+public class main { 
+	
+	public main (String[]args) {
+		crearArchivo("Hola.txt"); 
+		
+	}
+
+	private static void crearArchivo(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
